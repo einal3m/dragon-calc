0e4702963dc227de70f025e5fc62e1399fd2c93e
Melanie Matthews <melanie.a.matthews@gmail.com>
2017-01-13 22:38:27 +1100

    add resolve section to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  entry: "./src/entry.jsx",
+  entry: './src/entry.jsx',
   output: {
     path: __dirname,
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: "style!css" }
-    ]
-  }
+      { test: /\.css$/, loader: 'style!css' },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
 };
