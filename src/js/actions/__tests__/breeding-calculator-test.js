fd5c2880f0c0b0f59f18b719fa5b86e345f7ecdc
Melanie Matthews <melanie.a.matthews@gmail.com>
2017-01-17 10:33:57 +1100

    add basic breeding calc

--- src/js/actions/__tests__/breeding-calculator-test.js
+++ src/js/actions/__tests__/breeding-calculator-test.js
@@ -2,7 +2,20 @@ import breedingCalculator from '../breeding-calculator';
 import dragons from '../../data/dragons';
 
 describe('breedingCalculator', () => {
-  it('returns the left and right dragons', () => {
-    expect(breedingCalculator(1, 2)).toEqual([dragons[1], dragons[2]]);
+  describe('element matching', () => {
+    it('returns dragons whose elements are included in the right and left dragons', () => {
+
+      const jungle = 1;
+      const earth = 5;
+      const oasis = 8;
+      const underbrush = 9;
+
+      expect(breedingCalculator(1, 5)).toEqual(
+        [dragons[jungle], dragons[earth], dragons[oasis], dragons[underbrush]]
+      );
+      expect(breedingCalculator(5, 1)).toEqual(
+        [dragons[jungle], dragons[earth], dragons[oasis], dragons[underbrush]]
+      );
+    });
   });
 });
--- src/js/actions/breeding-calculator.js
+++ src/js/actions/breeding-calculator.js
@@ -1,5 +1,17 @@
 import dragons from '../data/dragons';
 
+const elements = (...elementArrays) => new Set([].concat(...elementArrays));
+
 export default function breedingCalculator(leftDragon, rightDragon) {
-  return [dragons[leftDragon], dragons[rightDragon]];
+  const selectedElements = elements(dragons[leftDragon].elements, dragons[rightDragon].elements);
+  const result = [];
+
+  dragons.forEach((dragon) => {
+    const matchingElements = dragon.elements.filter(element => selectedElements.has(element));
+    if (matchingElements.length === dragon.elements.length) {
+      result.push(dragon);
+    }
+  });
+
+  return result;
 }
